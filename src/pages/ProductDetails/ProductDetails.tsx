cbffd92faf93aa049cfd3c4a2c65b1880fd88578
Maks <maksimkakasimba@gmail.com>
2025-05-24 20:09:49 -0300

    пинаю детали

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -123,7 +123,6 @@ export default function ProductDetails({ chatId }: { chatId: number }) {
       <div className="image__container">
         <img
           src={productData?.images?.length ? productData?.images[0] : ""}
-          style={{ width: "100%", height: "100%", objectFit: "contain" }}
         />
         <div className="body">
           <p className="title">{productData.name}</p>
