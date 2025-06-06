82a8d1eb13dafbe4acb3006eb17e2d9e69799fe8
alexr <alexrub93@gmail.com>
2025-06-06 01:24:40 -0300

    https://vkusbot.ru/

--- src/components/Products/EveryProduct.tsx
+++ src/components/Products/EveryProduct.tsx
@@ -13,7 +13,7 @@ export default function EveryProduct({
   const [count, setCount] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
-  const url = "https://goodfood.vkusbot.ru/vkusnaya_argentina/shop/";
+  const url = "https://vkusbot.ru/vkusnaya_argentina/shop/";
 
   const getCartData = () => {
     setCount(
